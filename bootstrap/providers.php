aa4810c5da6f54dbdaa2dbd3485651200676c246
Arthur Sicard <arthur2rennes@hotmail.fr>
2025-08-10 21:20:43 +0200

    feat: configurer les providers blade-icons et ajouter la configuration

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,4 +2,7 @@
 
 return [
     App\Providers\AppServiceProvider::class,
+    Art35rennes\DaisyKit\DaisyKitServiceProvider::class,
+    BladeUI\Heroicons\BladeHeroiconsServiceProvider::class,
+    MallardDuck\BladeLucideIcons\BladeLucideIconsServiceProvider::class,
 ];
