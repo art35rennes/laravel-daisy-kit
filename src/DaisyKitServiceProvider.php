305f20ce6f0ea352faa062f1c3dca6ae0eec1650
Arthur Sicard <arthur2rennes@hotmail.fr>
2025-08-12 19:27:39 +0200

    feat: mettre à jour la page de démonstration et le service provider

--- src/DaisyKitServiceProvider.php
+++ src/DaisyKitServiceProvider.php
@@ -12,10 +12,18 @@ class DaisyKitServiceProvider extends ServiceProvider
         // Charger les vues du package avec un namespace: x-daisy::ui.button
         $this->loadViewsFrom(__DIR__.'/../resources/views', 'daisy');
 
+        // Charger les traductions du package: __('daisy::calendar.today')
+        $this->loadTranslationsFrom(__DIR__.'/../resources/lang', 'daisy');
+
         // Publication optionnelle des vues
         $this->publishes([
             __DIR__.'/../resources/views' => resource_path('views/vendor/daisy'),
         ], 'daisy-views');
+
+        // Publication optionnelle des traductions
+        $this->publishes([
+            __DIR__.'/../resources/lang' => resource_path('lang/vendor/daisy'),
+        ], 'daisy-lang');
     }
 }
 
