e7353bfb7d747b2e91068f7fe6bb5b86137f6929
Arthur Sicard <arthur2rennes@hotmail.fr>
2026-08-29 19:44:15 +0200

    style: preserve map test import spacing

--- tests/Feature/MapComponentTest.php
+++ tests/Feature/MapComponentTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 use Art35rennes\DaisyKit\Map\MapConfiguration;
 use Art35rennes\DaisyKit\Support\JsonConfiguration;
+
 function mapConfiguration(string $html): array
 {
     preg_match('/<script data-daisy-kit-config type="application\/json">(.*?)<\/script>/s', $html, $matches);
